03ba82a538561e0666b0f7da04961bc0ae5f4897
begum <bbegumozkisirlar@gmail.com>
2022-08-25 21:33:07 +0300

    test files

---
+++ ScrollView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ScrollView
+//
+//  Created by Beyza Begüm Özkısırlar on 13.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
